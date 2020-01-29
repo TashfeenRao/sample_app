834f24c6a1f955ea0524c95572191373d2631a3c
TashfeenRao <raotashfeen25@gmail.com>
2020-01-29 23:10:53 +0500

    finish editing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,2 @@
 class ApplicationController < ActionController::Base
-    def name
-        render html: 'My name is Tashfeen I am learning rails'
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root 'application#name'
+  root 'static_pages#home'
+  
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
 end
