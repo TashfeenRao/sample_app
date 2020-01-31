9d8ed2e868e3b555615a0ff644b0f29e34cbbd2f
TashfeenRao <raotashfeen25@gmail.com>
2020-02-01 03:49:33 +0500

    finished layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'static_pages#home'
@@ -6,4 +7,5 @@ Rails.application.routes.draw do
   get '/help',     to: 'static_pages#help'
   get '/about',    to: 'static_pages#about'
   get '/contact',  to: 'static_pages#contact'
+  get '/signup',   to: 'users#new'
 end
