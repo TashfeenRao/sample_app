2cbfb3a7113b3fb2469f4911b0db5f3dc8bd17e7
TashfeenRao <raotashfeen25@gmail.com>
2020-02-05 02:20:58 +0500

    validation and models added

---
+++ test/models/user_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(name:"tashfeen",email:"tashfeen@gamil.com")
+  end
+
+  test "should valid" do
+    assert @user.valid?
+  end
+
+  test "should have a name" do 
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "should have a email" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "should have valid email format" do 
+    invalid_addresses = %w[example@,com exmple.hello@com example@.org at_helo_.@.com at_@_helo@.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test "email should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+end
