ea48faa01823132bbc52f95efa1b6f237b95d64f
TashfeenRao <raotashfeen25@gmail.com>
2020-02-11 03:13:38 +0500

    finish sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-
+      flash[:success] = 'Welcome to App'
+      redirect_to @user
     else
       render "new"
     end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -16,4 +16,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       assert_template 'users/new'
     end
   end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
 end
