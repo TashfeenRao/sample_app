1ccedeb69866385a36406ccc727361fd7cf573a2
TashfeenRao <raotashfeen25@gmail.com>
2020-02-14 03:37:45 +0500

    login sessions added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
@@ -9,5 +10,8 @@ Rails.application.routes.draw do
   get '/contact',  to: 'static_pages#contact'
   get '/signup',   to: 'users#new'
   post '/signup',  to: 'users#create'
+  get '/login',    to: 'sessions#new'
+  post '/login',   to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :"users"
 end
