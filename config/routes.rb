68d4ad5985c87c237c3d8d467a958dc574057ca3
TashfeenRao <raotashfeen25@gmail.com>
2020-02-15 02:27:55 +0500

    logout bug fixed

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
   get '/login',    to: 'sessions#new'
   post '/login',   to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  get     '/logout', to: 'sessions#destroy'
   resources :"users"
 end
