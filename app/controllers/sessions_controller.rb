b3ffa3fb9f8e167b8b2b7bf212804f886d92bb71
TashfeenRao <raotashfeen25@gmail.com>
2020-02-18 00:58:11 +0500

    checkbox is completed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,17 +2,18 @@ class SessionsController < ApplicationController
   def new
   end
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user
     else
       flash.now[:danger] = "Invalid username/email"
     render 'new'
     end
   end
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_04_215026) do
+ActiveRecord::Schema.define(version: 2020_02_14_213537) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_02_04_215026) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
